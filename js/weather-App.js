f840a56eb9082bbc8346f7167e8b7b199356e918
Maarg2022 <116040849+Maarg2022@users.noreply.github.com>
2022-12-10 19:20:48 -0600

    Update weather-App.js

--- js/weather-App.js
+++ js/weather-App.js
@@ -53,6 +53,34 @@ window.addEventListener('load', () => {
         });
     }
 });
+// Current date
+const todaysDate = new Date();
+document.getElementById("CurrentDate").innerHTML = todaysDate;
+
+// Current time
+let clock = () => {
+    let date = new Date();
+    let hrs = date.getHours();
+    let mins = date.getMinutes();
+    let secs = date.getSeconds();
+    let period = "AM";
+    if (hrs == 0) {
+      hrs = 12;
+    } else if (hrs >= 12) {
+      hrs = hrs - 12;
+      period = "PM";
+    }
+    hrs = hrs < 10 ? "0" + hrs : hrs;
+    mins = mins < 10 ? "0" + mins : mins;
+    secs = secs < 10 ? "0" + secs : secs;
+  
+    let time = `${hrs}:${mins}:${secs}:${period}`;
+    document.getElementById("CurrentTime").innerText = time;
+    setTimeout(clock, 1000);
+  };
+  
+  clock();
+
 
 
 
